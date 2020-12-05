730caf318c4a1f2b2b0c9417d1c5d1e20c0efeb9
MaedehJJ <majj2000.mail@gmail.com>
2020-12-05 22:22:22 +0330

    Adding a toolbar to ui

--- app/src/main/java/com/example/travelplanner/list/ListFragment.kt
+++ app/src/main/java/com/example/travelplanner/list/ListFragment.kt
@@ -5,17 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.travelplanner.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
-        setHasOptionsMenu(true)
+        val view = inflater.inflate(R.layout.fragment_list , container , false)
+        val addNewPlace = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+
+        addNewPlace.setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
         return view
     }
 
