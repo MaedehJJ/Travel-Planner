aea6e72dc454bcd727e11bc23f926896eed02994
MaedehJJ <majj2000.mail@gmail.com>
2020-12-06 13:31:34 +0330

    getting add new places user interface ready

--- app/src/main/java/com/example/travelplanner/add/AddNewPlaces.kt
+++ app/src/main/java/com/example/travelplanner/add/AddNewPlaces.kt
@@ -3,10 +3,16 @@ package com.example.travelplanner.add
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.travelplanner.R
+import kotlinx.android.synthetic.main.activity_add_new_places.*
 
 class AddNewPlaces : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_places)
+        setSupportActionBar(myToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        myToolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
