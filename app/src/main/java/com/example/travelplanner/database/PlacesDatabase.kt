d7cdda629ae537745705abd6d13b8fcdc84264cd
MaedehJJ <majj2000.mail@gmail.com>
2020-12-08 19:09:18 +0330

    getting database class ready

---
+++ app/src/main/java/com/example/travelplanner/database/PlacesDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.travelplanner.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [PlacesData::class], version = 1, exportSchema = false)
+abstract class PlacesDatabase: RoomDatabase() {
+    abstract fun placesDao() : PlacesDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE : PlacesDatabase? = null
+        fun getDataBase(context: Context): PlacesDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PlacesDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+}
