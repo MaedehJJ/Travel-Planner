527e2e3b98a66822ebac35466a64efdf6b9f5f38
MaedehJJ <majj2000.mail@gmail.com>
2020-12-08 19:02:27 +0330

    getting data model of room database ready (PlacesData.kt)

---
+++ app/src/main/java/com/example/travelplanner/database/PlacesData.kt
@@ -0,0 +1,18 @@
+package com.example.travelplanner.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "places_table")
+data class PlacesData (
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "Title") var title: String,
+    @ColumnInfo(name = "Description") var description: String,
+    @ColumnInfo(name = "Image") var image: String,
+    @ColumnInfo(name = "Date") var date: String,
+    @ColumnInfo(name = "Location") var location: String,
+    @ColumnInfo(name = "Latitude") var latitude: Double,
+    @ColumnInfo(name = "Longitude") var langitude: Double,
+
+    )
