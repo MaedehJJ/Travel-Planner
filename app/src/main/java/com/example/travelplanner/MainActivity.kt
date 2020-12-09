dd9de6388e474b44d12cc9499c50b210ba002594
MaedehJJ <majj2000.mail@gmail.com>
2020-12-09 09:13:11 +0330

    Reading data fro DB

--- app/src/main/java/com/example/travelplanner/MainActivity.kt
+++ app/src/main/java/com/example/travelplanner/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.travelplanner
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.ActivityNavigator
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.travelplanner.add.AddNewPlaces
+import com.example.travelplanner.database.PlacesData
+import com.example.travelplanner.viewModel.PlacesViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mPlacesViewModel : PlacesViewModel by viewModels()
+    private lateinit var dataList: ArrayList<PlacesData>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +26,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this , AddNewPlaces::class.java))
             overridePendingTransition(R.anim.to_right, R.anim.from_left)
         }
+    }
+    private fun getPlacesListFromDB(){
+        mPlacesViewModel.getAllData.observe(this , Observer { data ->
+            dataList.addAll(data)
+        })
+
+
     }
 }
